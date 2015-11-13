75661aa1d0714a5dc4243ca9915e9daed984290e
zhongpei <zhongpei@vip.qq.com>
2015-11-13 21:05:31 +0800

    fix

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 sudo docker run  --name=registry \
-     -d \
      -p 5000:5000 \
-     -v $(pwd):/registry-conf \
-     -v /data/registry:/tmp/registry  \
-     -e STANDALONE=false \
-     -e DEBUG=true \
-     -e MIRROR_SOURCE=https://registry-1.docker.io \
-     -e MIRROR_SOURCE_INDEX=https://index.docker.io \
-     registry 
+     -v $(pwd)/config.yml:/var/lib/registry/config.yml:ro \
+     -v /data/registry:/var/lib/registry \
+     registry:2
