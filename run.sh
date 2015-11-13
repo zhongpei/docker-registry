b0152262fb7d62770ed67334372cb804b01cadaa
zhongpei <zhongpei@vip.qq.com>
2015-11-13 14:33:01 +0800

    fix

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 sudo docker run  --name=registry \
      -d \
      -p 5000:5000 \
-     -v {pwd}:/registry-conf \
+     -v $(pwd):/registry-conf \
      -v /data/registry:/tmp/registry  \
      -e STANDALONE=false \
      -e DEBUG=true \
