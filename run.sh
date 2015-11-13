901a52abd48a5ae0e278b2c4562f58839b67eb6b
zhongpei <zhongpei@vip.qq.com>
2015-11-13 21:25:42 +0800

    fix

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo docker run  --name=registry \
-     -p 5000:5000 \
-     -v $(pwd)/config.yml:/var/lib/registry/config.yml:ro \
-     -v /data/registry:/var/lib/registry \
-     registry:2
