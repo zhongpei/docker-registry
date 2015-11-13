2dd4f043290583c2edb6c392f0c2f0c9c9275969
zhongpei <zhongpei@vip.qq.com>
2015-11-13 14:21:29 +0800

    init

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo docker run  --name=registry \
+     -d \
+     -p 5000:5000 \
+     -v {pwd}:/registry-conf \
+     -v /data/registry:/tmp/registry  \
+     -e STANDALONE=false \
+     -e DEBUG=true \
+     -e MIRROR_SOURCE=https://registry-1.docker.io \
+     -e MIRROR_SOURCE_INDEX=https://index.docker.io \
+     registry 
